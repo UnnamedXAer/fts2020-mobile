beddaccd87785d01bb99226fe120c4d4db5cf426
Kamil <unnamedxaer@gmail.com>
2020-05-23 23:43:31 +0200

    Add temporarily button to logout

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -7,11 +7,12 @@ import FlatsScreen from '../screens/FlatsScreen';
 import LogInScreen from '../screens/LogInScreen';
 import RootState from '../store/storeTypes';
 import User from '../models/user';
-import { tryAuthorize } from '../store/actions/auth';
+import { tryAuthorize, logOut } from '../store/actions/auth';
 import LoadingScreen from '../screens/LoadingScreen';
 import RegistrationScreen from '../screens/RegistrationScreen';
 import FlatDetailsScreen from '../screens/FlatDetailsScreen';
 import { navigationContainerTheme } from '../config/theme';
+import Link from '../components/UI/Link';
 
 export type RootStackParamList = {
 	Flats: undefined;
@@ -21,20 +22,27 @@ export type RootStackParamList = {
 };
 
 const RootStack = createStackNavigator<RootStackParamList>();
-const RootStackNavigator = () => (
-	<RootStack.Navigator>
-		<RootStack.Screen
-			name="Flats"
-			options={{ title: 'Your Flats' }}
-			component={FlatsScreen}
-		/>
-		<RootStack.Screen
-			name="FlatDetails"
-			options={{ title: 'View Flat' }}
-			component={FlatDetailsScreen}
-		/>
-	</RootStack.Navigator>
-);
+const RootStackNavigator = () => {
+	const dispatch = useDispatch();
+	return (
+		<RootStack.Navigator
+			screenOptions={{
+				headerRight: (props) => <Link onPress={() => dispatch(logOut())}>L</Link>,
+			}}
+		>
+			<RootStack.Screen
+				name="Flats"
+				options={{ title: 'Your Flats' }}
+				component={FlatsScreen}
+			/>
+			<RootStack.Screen
+				name="FlatDetails"
+				options={{ title: 'View Flat' }}
+				component={FlatDetailsScreen}
+			/>
+		</RootStack.Navigator>
+	);
+};
 
 const AppNavitaionContainer = () => {
 	const [loading, setLoading] = useState(true);
@@ -55,7 +63,7 @@ const AppNavitaionContainer = () => {
 			setLoading(false);
 		}
 	}, [loggedUser]);
-	
+
 	return (
 		<NavigationContainer theme={navigationContainerTheme}>
 			{loading ? (

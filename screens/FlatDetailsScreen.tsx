050a2ed80931d69e627580cff463a1f430c98864
Kamil <unnamedxaer@gmail.com>
2020-06-07 20:01:11 +0200

    Add FAB to flat details
    Adds FAB element with action to navigate to invite members screen

--- screens/FlatDetailsScreen.tsx
+++ screens/FlatDetailsScreen.tsx
@@ -11,6 +11,7 @@ import {
 	Text,
 	List,
 	Avatar,
+	FAB,
 } from 'react-native-paper';
 import { Placeholder, PlaceholderLine, Shine } from 'rn-placeholder';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -35,6 +36,7 @@ const dimensions = Dimensions.get('screen');
 const FlatDetailsScreen: React.FC<Props> = ({ route, navigation }) => {
 	const theme = useTheme();
 	const dispatch = useDispatch();
+	const [fabOpen, setFabOpen] = useState(false);
 	const id = route.params.id;
 	const flat = useSelector((state: RootState) =>
 		state.flats.flats.find((x) => x.id === id)
@@ -111,114 +113,145 @@ const FlatDetailsScreen: React.FC<Props> = ({ route, navigation }) => {
 	};
 
 	return (
-		<ScrollView style={styles.screen}>
-			<Headline style={{ alignSelf: 'center', paddingTop: 24 }}>
-				{flat.name}
-			</Headline>
+		<>
+			<ScrollView style={styles.screen}>
+				<Headline style={{ alignSelf: 'center', paddingTop: 24 }}>
+					{flat.name}
+				</Headline>
 
-			<View style={[styles.section, styles.infoContainer]}>
-				<View style={styles.avatarContainer}>
+				<View style={[styles.section, styles.infoContainer]}>
+					<View style={styles.avatarContainer}>
+						<View
+							style={[
+								{ backgroundColor: theme.colors.disabled },
+								styles.avatar,
+							]}
+						>
+							<MaterialCommunityIcons
+								name="home-city-outline"
+								size={40}
+								color={theme.colors.background}
+							/>
+						</View>
+					</View>
 					<View
-						style={[
-							{ backgroundColor: theme.colors.disabled },
-							styles.avatar,
-						]}
+						style={{
+							minWidth: (() => {
+								const x = dimensions.width - 32 - 16 - 64;
+								return x;
+							})(),
+
+							marginStart: 8,
+							justifyContent: 'center',
+						}}
 					>
-						<MaterialCommunityIcons
-							name="home-city-outline"
-							size={40}
-							color={theme.colors.background}
-						/>
+						{flat.owner ? (
+							<>
+								<View
+									style={{
+										flexDirection: 'row',
+										flexWrap: 'wrap',
+										justifyContent: 'flex-start',
+									}}
+								>
+									<Text>Created by </Text>
+									<Link
+										onPress={() => personClickHandler(flat.owner!.id)}
+									>
+										{flat.owner.emailAddress}
+									</Link>
+								</View>
+								<Text>
+									Created at {moment(flat.createAt).format('ll')}
+								</Text>
+							</>
+						) : (
+							<Placeholder Animation={Shine}>
+								<PlaceholderLine height={16} />
+								<PlaceholderLine height={16} />
+							</Placeholder>
+						)}
 					</View>
 				</View>
-				<View
-					style={{
-						minWidth: (() => {
-							const x = dimensions.width - 32 - 16 - 64;
-							return x;
-						})(),
-
-						marginStart: 8,
-						justifyContent: 'center',
-					}}
-				>
-					{flat.owner ? (
-						<>
-							<View
-								style={{
-									flexDirection: 'row',
-									flexWrap: 'wrap',
-									justifyContent: 'flex-start',
-								}}
-							>
-								<Text>Created by </Text>
-								<Link onPress={() => personClickHandler(flat.owner!.id)}>
-									{flat.owner.emailAddress}
-								</Link>
-							</View>
-							<Text>Created at {moment(flat.createAt).format('ll')}</Text>
-						</>
+				<Divider style={styles.divider} />
+				<View style={styles.section}>
+					<Title>Description</Title>
+					<Paragraph>{flat.description}</Paragraph>
+				</View>
+				<Divider style={styles.divider} />
+				<View style={styles.section}>
+					<Title>Members</Title>
+					{flat.members ? (
+						flat.members.map((member) => {
+							return (
+								<List.Item
+									key={member.id}
+									title={member.emailAddress}
+									description={member.userName}
+									left={(props) =>
+										member.avatarUrl ? (
+											<Avatar.Image
+												source={{
+													uri: member.avatarUrl,
+												}}
+												size={48}
+											/>
+										) : (
+											<Avatar.Icon
+												icon="account-outline"
+												size={48}
+												theme={{
+													colors: {
+														primary: theme.colors.disabled,
+													},
+												}}
+											/>
+										)
+									}
+									rippleColor={theme.colors.primary}
+									onPress={() => memberSelectHandler(member.id)}
+								/>
+							);
+						})
 					) : (
 						<Placeholder Animation={Shine}>
-							<PlaceholderLine height={16} />
-							<PlaceholderLine height={16} />
+							<PlaceholderLine height={40} />
+							<PlaceholderLine height={40} />
 						</Placeholder>
 					)}
 				</View>
-			</View>
-			<Divider style={styles.divider} />
-			<View style={styles.section}>
-				<Title>Description</Title>
-				<Paragraph>{flat.description}</Paragraph>
-			</View>
-			<Divider style={styles.divider} />
-			<View style={styles.section}>
-				<Title>Members</Title>
-				{flat.members ? (
-					flat.members.map((member) => {
-						return (
-							<List.Item
-								key={member.id}
-								title={member.emailAddress}
-								description={member.userName}
-								left={(props) =>
-									member.avatarUrl ? (
-										<Avatar.Image
-											source={{
-												uri: member.avatarUrl,
-											}}
-											size={48}
-										/>
-									) : (
-										<Avatar.Icon
-											icon="account-outline"
-											size={48}
-											theme={{
-												colors: {
-													primary: theme.colors.disabled,
-												},
-											}}
-										/>
-									)
-								}
-								rippleColor={theme.colors.primary}
-								onPress={() => memberSelectHandler(member.id)}
-							/>
-						);
-					})
-				) : (
-					<Placeholder Animation={Shine}>
-						<PlaceholderLine height={40} />
-						<PlaceholderLine height={40} />
-					</Placeholder>
-				)}
-			</View>
-			<Divider style={styles.divider} />
-			<View style={styles.section}>
-				<Title>Tasks</Title>
-				<FlatTasksList flatId={id!} navigation={navigation} />
-			</View>
-		</ScrollView>
+				<Divider style={styles.divider} />
+				<View style={styles.section}>
+					<Title>Tasks</Title>
+					<FlatTasksList flatId={id!} navigation={navigation} />
+				</View>
+			</ScrollView>
+			<FAB.Group
+				visible={Boolean(flat.members && flat.owner)}
+				open={fabOpen}
+				color="white"
+				icon={fabOpen ? 'close' : 'plus'}
+				actions={[
+					{
+						icon: 'account-multiple-plus',
+						onPress: () =>
+							navigation.navigate('InviteMembers', {
+								flatId: id,
+								isNewFlat: false,
+							}),
+						label: 'Invite a new member',
+					},
+				]}
+				onStateChange={({ open }) => {
+					setFabOpen(open);
+				}}
+				onPress={() => {
+					if (fabOpen) {
+						// do something if the speed dial is open
+					}
+				}}
+			/>
+		</>
 	);
 };
 

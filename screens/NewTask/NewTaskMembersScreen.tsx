6dc1ede6d33537983a765f65a62a886b9bcc5352
Kamil <unnamedxaer@gmail.com>
2020-06-27 20:29:04 +0200

    Update new task members screen

--- screens/NewTask/NewTaskMembersScreen.tsx
+++ screens/NewTask/NewTaskMembersScreen.tsx
@@ -8,20 +8,18 @@ import {
 	TouchableWithoutFeedback,
 	Keyboard,
 } from 'react-native';
-import { withTheme, List, Avatar, IconButton } from 'react-native-paper';
+import { withTheme, List, Avatar, IconButton, Chip, Text } from 'react-native-paper';
 import { Theme } from 'react-native-paper/lib/typescript/src/types';
 import {
 	NewTaskMembersScreenNavigationProps,
 	NewTaskMembersScreenRouteProps,
 } from '../../types/navigationTypes';
 import { StateError } from '../../store/ReactTypes/customReactTypes';
-import { TaskData } from '../../models/task';
 import HttpErrorParser from '../../utils/parseError';
 import Header from '../../components/UI/Header';
 import NotificationCard from '../../components/UI/NotificationCard';
 import CustomButton from '../../components/UI/CustomButton';
 import RootState from '../../store/storeTypes';
-import { Item } from 'react-native-paper/lib/typescript/src/components/Drawer/Drawer';
 import User from '../../models/user';
 import Stepper from '../../components/UI/Stepper';
 
@@ -43,9 +41,7 @@ const NewTaskMembersScreen: React.FC<Props> = ({ theme, navigation, route }) => 
 		(state: RootState) =>
 			state.flats.flats.find((x) => x.id === task.flatId)!.members!
 	);
-	const [addedMembers, setAddedMembers] = useState([
-		flatMembers.find((x) => x.id === task.createBy!)!,
-	]);
+	const [addedMembers, setAddedMembers] = useState([...flatMembers!]);
 
 	const isMounted = useRef(true);
 	useEffect(() => {
@@ -68,6 +64,8 @@ const NewTaskMembersScreen: React.FC<Props> = ({ theme, navigation, route }) => 
 	const submitHandler = async () => {
 		setError(null);
 		let isFormValid = true;
+
+		
 	};
 
 	return (
@@ -86,54 +84,99 @@ const NewTaskMembersScreen: React.FC<Props> = ({ theme, navigation, route }) => 
 					<Stepper steps={3} currentStep={2} />
 					<Header style={styles.header}>Set Task - Members</Header>
 					<View style={styles.inputContainer}>
-						<List.Section title="Flat members">
-							{flatMembers.map((member) => (
-								<List.Item
-									key={member.id}
-									title={member.emailAddress}
-									description={member.userName}
-									left={() =>
-										member.avatarUrl ? (
-											<Avatar.Image
-												size={48}
-												style={{
-													width: 48,
-													height: 48,
-													marginHorizontal: 0,
-												}}
-												source={{ uri: member.avatarUrl }}
-											/>
-										) : (
-											<Avatar.Icon
-												color={theme.colors.primary}
-												icon="account-outline"
-												size={48}
-												theme={{
-													colors: {
-														primary: theme.colors.background,
-													},
-												}}
-												style={{
-													width: 48,
-													height: 48,
-													marginHorizontal: 0,
-												}}
-											/>
-										)
-									}
-									right={() => (
-										<IconButton
-											color={theme.colors.placeholder}
-											icon="plus"
-											disabled={loading}
-											onPress={() => {
-												addMemberHandler(member.id);
-											}}
+						<List.Section title="Unassigned flat members">
+							{flatMembers.length !== addedMembers.length ? (
+								flatMembers
+									.filter((x) => !addedMembers.includes(x))
+									.map((member) => (
+										<List.Item
+											key={member.id}
+											title={member.emailAddress}
+											description={member.userName}
+											left={() =>
+												member.avatarUrl ? (
+													<Avatar.Image
+														size={48}
+														style={{
+															width: 48,
+															height: 48,
+															marginHorizontal: 0,
+														}}
+														source={{ uri: member.avatarUrl }}
+													/>
+												) : (
+													<Avatar.Icon
+														color={theme.colors.primary}
+														icon="account-outline"
+														size={48}
+														theme={{
+															colors: {
+																primary:
+																	theme.colors
+																		.background,
+															},
+														}}
+														style={{
+															width: 48,
+															height: 48,
+															marginHorizontal: 0,
+														}}
+													/>
+												)
+											}
+											right={() => (
+												<IconButton
+													color={theme.colors.placeholder}
+													icon="plus"
+													disabled={loading}
+													onPress={() => {
+														addMemberHandler(member.id);
+													}}
+												/>
+											)}
 										/>
-									)}
-								/>
-							))}
+									))
+							) : (
+								<NotificationCard serverity="info">
+									All members are assigned to task.
+								</NotificationCard>
+							)}
 						</List.Section>
+					</View>
+					<View style={styles.inputContainer}>
+						<Text>Assigned people</Text>
+					</View>
+					<View style={[styles.inputContainer, styles.addedMembers]}>
+						{addedMembers.map((member) => (
+							<Chip
+								key={member.id}
+								style={styles.chip}
+								textStyle={
+									member.id === task.createBy
+										? { color: theme.colors.placeholder }
+										: void 0
+								}
+								avatar={
+									member.avatarUrl ? (
+										<Avatar.Image
+											source={{ uri: member.avatarUrl }}
+											size={24}
+										/>
+									) : (
+										<Avatar.Icon icon="account" size={24} />
+									)
+								}
+								mode="flat"
+								onClose={
+									member.id !== task.createBy
+										? () => removeMemberHandler(member.id)
+										: void 0
+								}
+							>
+								{member.id === task.createBy && '[You] '}
+								{member.emailAddress}
+							</Chip>
+						))}
 					</View>
 					<View style={styles.inputContainer}>
 						{error && (
@@ -179,8 +222,14 @@ const styles = StyleSheet.create({
 		maxWidth: 400,
 		marginVertical: 4,
 	},
-	input: {
-		fontSize: 16,
+	addedMembers: {
+		flexWrap: 'wrap',
+		flexDirection: 'row',
+	},
+	chip: {
+		marginRight: 8,
+		marginBottom: 4,
+		flexWrap: 'nowrap',
 	},
 	actions: {
 		flexDirection: 'row',

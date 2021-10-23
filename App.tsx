fecd3c8c8f3b8d9da3350ec030b9257638164d10
UnnamedXAer <unnamedxaer.git@gmail.com>
2021-10-23 20:41:23 +0200

    fixing launch problems

--- App.tsx
+++ App.tsx
@@ -1,18 +1,46 @@
-import React, { useState } from 'react';
-import { AppLoading } from 'expo';
+import React, { useEffect, useState } from 'react';
 import { Provider as StoreProvider } from 'react-redux';
 import { Provider as PaperProvider } from 'react-native-paper';
 import AppNavigationContainer from './navigation/Navigation';
+// import * as SplashScreen from 'expo-splash-screen';
 import store from './store/store';
 import { paperTheme } from './config/theme';
-
+import AppLoading from 'expo-app-loading';
+// console.log('0000 ', SplashScreen);
 export default function App() {
 	const [loading, setLoading] = useState(true);
 
+	const init = async () => {};
+
+	useEffect(() => {
+		init();
+	}, []);
+
 	if (loading) {
-		return (
-			<AppLoading startAsync={async () => {}} onFinish={() => setLoading(false)} />
-		);
+		return <AppLoading autoHideSplash />;
+	}
+
+	// if (loading) {
+	// 	return (
+	// 		<AppLoading startAsync={async () => {}} onFinish={() => setLoading(false)} />
+	// 	);
+	// }
+
+	// const init = async () => {
+	// 	try {
+	// 		// Keep on showing the SlashScreen
+	// 		await SplashScreen.preventAutoHideAsync();
+	// 	} catch (err) {
+	// 		console.log('splashScreen error:', err);
+	// 	} finally {
+	// 		setLoading(true);
+	// 		// Hiding the SplashScreen
+	// 		await SplashScreen.hideAsync();
+	// 	}
+	// };
+
+	if (loading) {
+		return null;
 	}
 
 	return (
